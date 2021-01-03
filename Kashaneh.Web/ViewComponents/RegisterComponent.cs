d8edc69188da6ee1a345fdca04e50427ceefafd2
Niloofar <nikitmb2@gmail.com>
2021-01-03 22:38:50 +0330

    register view component

---
+++ Kashaneh.Web/ViewComponents/RegisterComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kashaneh.Core.Convertors;
+using Kashaneh.Core.DTOs;
+using Kashaneh.Core.Generator;
+using Kashaneh.Core.Security;
+using Kashaneh.Core.Senders;
+using Kashaneh.Core.Services.Interfaces;
+using Kashaneh.DataLayer.Entities.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kashaneh.Web.ViewComponents
+{
+    public class RegisterComponent : ViewComponent
+    {
+        private IUserService _userService;
+        private IViewRenderService _viewRender;
+
+        public RegisterComponent(IUserService userService, IViewRenderService viewRender)
+        {
+            _userService = userService;
+            _viewRender = viewRender;
+        }
+
+        [HttpGet]
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("_Register"));
+        }
+
+    }
+}
